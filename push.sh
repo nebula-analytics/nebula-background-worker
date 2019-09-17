6ab49eb5b6b44abb9dcd85b4dbf7d031adfd029b
Jeff Phan <s3672747@student.rmit.edu.au>
2019-09-17 22:28:25 +1000

    testing travis file, try to authenticate github

--- push.sh
+++ push.sh
@@ -1,32 +1,9 @@
 #!/usr/bin/env bash
 # Push HTML files to gh-pages automatically.
 
-
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
-case $key in
-    -u|--username)
-    USERNAME="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -p|--password)
-    PASSWORD="$2"
-    shift # past argument
-    shift # past value
-    ;;
-esac
-done
-set -- "${POSITIONAL[@]}" # restore positional
-
-
 # Fill this out with the correct org/repo
 ORG=nebula-analytics
 REPO=nebula-background-worker
-USRNAME=jeffphan8816
 # This probably should match an email for one of your users.
 EMAIL=s3672747@student.rmit.edu.au
 
@@ -34,7 +11,7 @@ set -e
 
 # Clone the gh-pages branch outside of the repo and cd into it.
 cd ..
-git clone -b gh-pages "https://$USERNAME:$PASSWORD@github.com/$ORG/$REPO.git" gh-pages
+git clone -b gh-pages "https://$USERNAME:$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
 cd gh-pages
 
 # Update git configuration so I can push.
